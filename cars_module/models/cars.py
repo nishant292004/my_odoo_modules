23c591f878dca0e5981f7c6b76404ce0cfd49828
nishant290 <nishantsuthar2906@gmail.com>
2025-03-18 14:20:58 +0530

    new color changes are made

--- cars_module/models/cars.py
+++ cars_module/models/cars.py
@@ -3,12 +3,13 @@ from odoo import models,fields
 class CarsDetails(models.Model):
     _name = 'cars.management'
     _description = 'Cars Management'
+    _rec_name = 'car_name'
 
     car_name = fields.Char('Car Name')
     company_name = fields.Char('Company Name')
 
     # engine_id = fields.Many2one(comodel_name='school.standard', string='Engine')
-    color = fields.Char('Color')
+    color = fields.Integer('Color')
     manufacturing_date = fields.Date('Manufacture Date')
 
     engine_id = fields.Many2one('cars.engine','Engine')
