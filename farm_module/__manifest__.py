c3d1e1d5cebf6796def13b83f5a7fab1498c2fd5
nishant290 <nishantsuthar2906@gmail.com>
2025-03-24 15:40:42 +0530

    [ADD]: New Farm Module Is Added

---
+++ farm_module/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name':'Farm Management', 
+    'summary':'This module will be used for Farm Management', 
+    'description':"""This module will manage all the information of farming.""", 
+    'version':'1.0', 
+    'category':'Farming', 
+    'author':'Nishant', 
+    'depends': ['base'], 
+    'data':[
+       
+    ], 
+    'sequence':10, 
+    'auto_install': False, 
+    'installable': True, 
+    'application': True, 
+    'license': 'LGPL-3', 
+}
---
+++ farm_module/models/farm.py
@@ -0,0 +1,5 @@
+from odoo import models,fields
+
+class MyFarm(models.Model):
+    _name = 'farm.management'
+    _description = 'Farm Model'
