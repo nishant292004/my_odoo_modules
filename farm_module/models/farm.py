0106d719f06a4b68df770e74d409564b1ccb1907
nishant290 <nishantsuthar2906@gmail.com>
2025-03-25 10:41:39 +0530

    [ADD]: new fields are added in the farm model

--- farm_module/models/farm.py
+++ farm_module/models/farm.py
@@ -1,5 +1,13 @@
 from odoo import models,fields
 
 class MyFarm(models.Model):
-    _name = 'farm.management'
+    _name = 'farm.model'
     _description = 'Farm Model'
+
+    farmer_name = fields.Char("Farmer's Name")
+    farm_size = fields.Float("Area Of Farm (Hectares)")
+    is_organic = fields.Boolean('Is Organic ?')
+    description = fields.Text('Description')
+    est_date = fields.Date('Establishment Date')
+    last_inspection = fields.Datetime('Last Inspection Date')
+    farm_type = fields.Selection([('small','Small'),('medium','Medium'),('large','Large')],String='Farm Type')
