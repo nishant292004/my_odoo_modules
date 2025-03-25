d394edef3a8ab59e1814d85a670b29528a490295
nishant290 <nishantsuthar2906@gmail.com>
2025-03-25 11:52:39 +0530

    [ADD]: required attribute is added in python and xml

--- farm_module/models/farm.py
+++ farm_module/models/farm.py
@@ -6,10 +6,10 @@ class MyFarm(models.Model):
     _rec_name = 'farmer_name'
     _order = 'farmer_name asc'
 
-    farmer_name = fields.Char("Farmer's Name")
-    farm_size = fields.Float("Area Of Farm (Hectares)")
+    farmer_name = fields.Char("Farmer's Name", required=True)
+    farm_size = fields.Float("Area Of Farm (Hectares)",required=True)
     is_organic = fields.Boolean('Is Organic ?')
     description = fields.Text('Description')
-    est_date = fields.Date('Establishment Date')
-    last_inspection = fields.Datetime('Last Inspection Date')
-    farm_type = fields.Selection([('small','Small'),('medium','Medium'),('large','Large')],string='Farm Type')
+    est_date = fields.Date('Establishment Date',required=True)
+    last_inspection = fields.Datetime('Last Inspection Date',required=True)
+    farm_type = fields.Selection([('small','Small'),('medium','Medium'),('large','Large')],string='Farm Type',required=True)
