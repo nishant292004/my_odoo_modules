8a1f756af621554ff699123c2d00bdbf745200bc
nishant290 <nishantsuthar2906@gmail.com>
2025-03-26 12:07:55 +0530

    [ADD]: new 2 self-calc float fields are added

--- farm_module/models/crop.py
+++ farm_module/models/crop.py
@@ -1,4 +1,4 @@
-from odoo import models,fields
+from odoo import models,fields,api
 
 class FarmCrop(models.Model):
     _name = 'farm.crop'
@@ -8,6 +8,35 @@ class FarmCrop(models.Model):
     crop_type = fields.Selection([('monsoon','Monsoon Season'),('winter','Winter Season'),('short','Short Season')],string='Type Of Crop')
     code = fields.Char(string='Code',size=4)
     farm_ids = fields.One2many(comodel_name='farm.model',inverse_name='crop_id',string='Farm')
-    cost = fields.Float('cost')
+    cost = fields.Float('Cost (Rs.)')
     govt_add = fields.Float("Government's Profit Margin (%)")
     mrkt = fields.Float("Market's Profit Margin (%) ")
+
+    govt_price = fields.Float(compute='_calc_govt_price', string='Government Price (Rs.)')
+    mrkt_price = fields.Float(compute='_calc_mrkt_price', string='Market Price (Rs.)')
+
+    @api.depends('cost','govt_add')
+
+    def _calc_govt_price(self):
+
+        """
+        This method is used to calculate the total govt price based on cost and govt profit margin.
+        -------------------------------------------------------------------------------------------
+        @param self: object pointer
+        """
+
+        for price in self:
+            price.govt_price = price.cost + (price.cost * price.govt_add)/100
+
+    @api.depends('cost','mrkt')
+
+    def _calc_mrkt_price(self):
+        """
+        This method is used to calculate the total market price based on cost and market profit margin.
+        -------------------------------------------------------------------------------------------
+        @param self: object pointer
+        """
+
+        for price in self:
+            price.mrkt_price = price.cost + (price.cost * price.mrkt)/100
+              
