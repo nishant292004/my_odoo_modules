63c071949f1c3d6bd332d1364a342641fc963223
nishant290 <nishantsuthar2906@gmail.com>
2025-03-26 10:47:34 +0530

    [ADD]: new ondelete restrict is created

--- farm_module/models/farmer.py
+++ farm_module/models/farmer.py
@@ -7,5 +7,6 @@ class Farmer(models.Model):
     name = fields.Char("Farmer's Name")
     age = fields.Integer("Age")
     mo_no = fields.Char("Mobile no.")
+    farm_id = fields.Many2one(comodel_name='farm.model', string='Farm Code', ondelete="restrict")
     crop_ids = fields.Many2many(comodel_name='farm.crop',string="Types Of Crops")
     machine_ids = fields.Many2many(comodel_name='farm.machines',relation='machine_farmer_rel',column1='machines_id',column2='farmer_id',string="Machines used in farm")
