a6ab6afe830fdefedd87dc7014e73a38f288294b
nishant290 <nishantsuthar2906@gmail.com>
2025-04-14 18:40:30 +0530

    [ADD]: unlink method is added

--- farm_module/models/farm.py
+++ farm_module/models/farm.py
@@ -407,3 +407,20 @@ class MyFarm(models.Model):
         new_crop = self.write(new_rec)
         print("O2M UPDATED",new_crop)  
 
+    def unlink_o2m_rec(self):
+        
+        """
+        This method is used to delete existing O2M records but not from database .
+        -------------------------------------------------------------------------
+        @param self: object pointer
+        """  
+          
+        new_rec =  {
+            'crop_ids':[
+                (3,1),
+                (Command.unlink(2))],
+            
+        } 
+        new_crop = self.write(new_rec)
+        print("O2M UPDATED",new_crop)  
+
