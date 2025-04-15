22062b47dffa063c8488feaf38354a6e5cc767e8
nishant290 <nishantsuthar2906@gmail.com>
2025-04-15 13:02:27 +0530

    [ADD]:offset is added

--- farm_module/models/machines.py
+++ farm_module/models/machines.py
@@ -178,5 +178,5 @@ class FarmMachines(models.Model):
         @param self: object pointer
         """
         farm_obj = self.env['farm.model']
-        all_recs = farm_obj.search([('farm_type','=','large')])
+        all_recs = farm_obj.search(domain=[('farm_type','=','large')],offset=2)
         print("ALL RECORDS OF FARM MODEL",all_recs)                                        
